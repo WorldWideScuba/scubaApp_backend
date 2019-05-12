804fad6f93da1d1c9cad8c76ebf6a34405fed931
chrismontyUT <chrismonty@utexas.edu>
2019-05-12 14:53:18 -0500

    Initial commit spring boot app

---
+++ src/main/java/com/scubaworld/scubagear/ScubaGearApplication.java
@@ -0,0 +1,13 @@
+package com.scubaworld.scubagear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScubaGearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScubaGearApplication.class, args);
+    }
+
+}
